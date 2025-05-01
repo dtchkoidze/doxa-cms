0cf7f72bf9c8136c0bb893858321ca810e986d5a
dtchkoidze <dtchkoiddze@gmail.com>
2025-05-01 19:24:07 +0400

    removed socket srv

--- Doxa/socket-srv/controllers/MessageController.js
+++
@@ -1,9 +0,0 @@
-import pool from "../db/db.js";
-
-class MessageController {
-    async handleMessage(msg) {
-        console.log("Message handling in controller: ", msg);
-    }
-}
-
-export default new MessageController();
