ff8c774d35935140da59bed3d043bb9b499c8445
Geperson <dave.servicemail@gmail.com>
2026-01-31 15:45:52 +0400

    Small fixes in emails

--- Doxa/User/src/Resources/views/emails/account-deletion-email.blade.php
+++ Doxa/User/src/Resources/views/emails/account-deletion-email.blade.php
@@ -1,21 +1,21 @@
 @component('mail::message')
-# Recovering Password!
+# Account Deletion!
 
 You are receiving this email because we received an account deletion request.
 
 Please note that once your account is deleted, it cannot be restored.
 
-Please verify your email address by clicking the button below or copying and pasting the secret code into the verification field.
+Please verify your email address by clicking the button below or copying and pasting the secret code into the
+verification field.
 
 Your Email: {{ $email }}
 
 **Secret:** {{ $secret }}
 
 @component('mail::button', ['url' => $verification_link])
-Verify Email and Delete Account
+    Verify Email and Delete Account
 @endcomponent
 
 
 If you did not request account deleteion, no further action is required, ignore this email.
-
 @endcomponent
